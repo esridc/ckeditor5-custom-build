86e8ca1513614d457b188ba64bb22d64ee1cff46
Randy Weber <rweber@esri.com>
2022-07-12 21:52:34 -0400

    Add mention plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,14 +3,15 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 
 class ClassicEditor extends ClassicEditorBase {}
 
@@ -22,7 +23,8 @@ const PLUGINS = {
 	Link,
 	Paragraph,
 	PasteFromOffice,
-	TextTransformation
+	TextTransformation,
+	Mention
 };
 
 const defaultPlugins = Object.values(PLUGINS);
