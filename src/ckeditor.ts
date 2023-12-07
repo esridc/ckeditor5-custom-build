b87a705518ca229c2f8bff0574c5c8336b751f98
Ben Stoltz <bstoltz@esri.com>
2023-12-07 10:24:38 -0500

    feat: add new plugins && update things to the new output format from ckeditor

---
+++ src/ckeditor.ts
@@ -0,0 +1,69 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { Mention } from '@ckeditor/ckeditor5-mention';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		BlockQuote,
+		Bold,
+		Essentials,
+		Heading,
+		Indent,
+		Italic,
+		Link,
+		List,
+		Mention,
+		Paragraph,
+		PasteFromOffice,
+		RemoveFormat,
+		TextTransformation
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'blockQuote',
+				'removeFormat',
+				'link',
+				'|',
+				'bulletedList',
+				'numberedList',
+				'alignment',
+				'outdent',
+				'indent',
+				'|',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'en'
+	};
+}
+
+export default Editor;
